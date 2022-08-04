cd120ee01f4e086ac5ce7c5c393b845575d3eab1
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-04 16:22:17 +0100

    Make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post </a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
+            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
+            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
+        </p>
+    </article>    
+
+    <a href="/">Go back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post </a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
+            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
+            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
+        </p>
+    </article>    
+
+    <article>
+    <h1><a href="/post">My Second Post </a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
+            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
+            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
+        </p>
+    </article>    
+
+    <article>
+    <h1><a href="/post">My Third Post </a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
+            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
+            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
+        </p>
+    </article>    
+</body>
+</html>
