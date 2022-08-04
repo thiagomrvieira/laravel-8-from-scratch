4cad896f731e2ee3d8403a7b4a1d3a04b24733dd
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-04 15:51:50 +0100

    Include CSS and JavaScript

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"> </script>
+</head>
+<body>
+    <h1>Hello world</h1>
+    
+</body>
+</html>
