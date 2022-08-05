a64d1c0be92f99b590a424c44173cad5147e9608
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-05 17:45:07 +0100

    Collection Sorting and Caching Refresher

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,17 +24,20 @@ class Post
 
     public static function all()
     {
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path('posts')))
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($document) => new Post(
+                        $document->title,
+                        $document->excerpt,
+                        $document->date,
+                        $document->body(),
+                        $document->slug
+                ))
+                ->sortByDesc('date');
+        });
 
-        return 
-            collect(File::files(resource_path('posts')))
-            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn ($document) => new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-            ));
+        
 
     }
 }
