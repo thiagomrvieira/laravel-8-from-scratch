49dc688f694936f48c4b88f4bd1f2976f6f2f69b
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-05 16:06:06 +0100

    Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,34 +5,10 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post </a></h1>
-
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
-            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
-            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
-        </p>
-    </article>    
-
-    <article>
-    <h1><a href="/posts/my-second-post">My Second Post </a></h1>
-
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
-            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
-            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
-        </p>
-    </article>    
-
-    <article>
-    <h1><a href="/posts/my-third-post">My Third Post </a></h1>
-
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam eveniet ad provident debitis beatae. Explicabo debitis asperiores quo accusantium magni corporis soluta quia modi atque fugiat. Iste aperiam quo doloremque.
-            Voluptatem laudantium repellat, culpa blanditiis vero asperiores natus eos, quidem laborum esse eveniet ducimus, veritatis id libero ut a eum in atque accusantium iusto earum tenetur vel rerum! Hic, mollitia.
-            Sunt quisquam reiciendis explicabo nihil laudantium fugiat consequuntur alias, impedit ducimus cupiditate id. Autem excepturi exercitationem voluptates dolorum voluptas eius aut neque, nulla facilis id quas eligendi voluptatem cupiditate vitae?
-        </p>
-    </article>    
+    @foreach ($posts as $post)
+        <article>
+            {!! $post !!}
+        </article>    
+    @endforeach
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,22 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts',[
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug) {
 
-    if (!file_exists( $path = __DIR__ . "/../resources/posts/{$slug}.html" ) ) {
-        return redirect('/');
-    }
-    
-    // $post = cache()->remember("posts.{$slug}", 3600, function() use($path) {
-    //     return file_get_contents($path);
-    // });
-
-    //  The same in php 7
-    $post = cache()->remember("posts.{$slug}", 3600, fn() => file_get_contents($path));
-    
-    return view('post', ['post' => $post]);
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
 
 })->where('post', '[A-z_\-]+');
