7c7cbdf1dfb8b081c18c6d68006bb004ec445f87
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-16 11:41:04 +0100

    Make a Post Model and Migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,50 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-class Post 
+class Post extends Model
 {
-    public function __construct(
-        public $title, 
-        public $excerpt, 
-        public $date, 
-        public $body, 
-        public $slug)
-    { }
-
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path('posts')))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn ($document) => new Post(
-                        $document->title,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                ))
-                ->sortByDesc('date');
-        });
-    }
+    use HasFactory;
 }
