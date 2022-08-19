2ce8ef0b7e91f0a0dc58f0904400ea97a37475cd
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-19 18:12:44 +0100

    Blade Components and CSS Grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -19,17 +19,17 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center"> No posts yet</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card /> 
-            <x-post-card /> 
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
+        {{-- <div class="lg:grid lg:grid-cols-3">
            <x-post-card /> 
            <x-post-card /> 
            <x-post-card /> 
-        </div>
+        </div> --}}
     </main>
 
 </x-layout>
