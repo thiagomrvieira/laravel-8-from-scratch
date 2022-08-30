965596815ab77adc81918914a496dcf77c661668
thiagomrvieira <thiagomrvieira@gmail.com>
2022-08-30 17:46:18 +0100

    Automatic Password Hashing With Mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class);
     }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
