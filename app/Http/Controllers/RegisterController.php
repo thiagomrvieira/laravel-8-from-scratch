fcbf3ff1f81e6d4563666977e683f192220193c9
thiagomrvieira <thiagomrvieira@gmail.com>
2022-10-17 20:09:41 +0100

    Show a Success Flash Message

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -23,6 +23,6 @@ class RegisterController extends Controller
 
         User::create($attributes);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Your Account has been created');
     }
 }
