e0fa4364549ec842242fb5b4f12c31d1b27e1793
thiagomrvieira <thiagomrvieira@gmail.com>
2022-10-17 19:53:02 +0100

    Failed Validation and Old Input Data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -17,8 +17,13 @@
                            type="text" 
                            name="name" 
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1"> {{ $message }}</p>
+                    @enderror
+
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -31,8 +36,12 @@
                            type="text" 
                            name="username" 
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1"> {{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -45,8 +54,12 @@
                            type="email" 
                            name="email" 
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1"> {{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -61,6 +74,9 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1"> {{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit"
