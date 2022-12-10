fb10eeb8bcb6d9f58ecc77cd171e9f1358033564
thiagomrvieira <thiagomrvieira@gmail.com>
2022-12-10 12:24:07 +0000

    Build the login page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+            return redirect('/')->with('success', 'Welcome back');
+        }
+        
+        throw ValidationException::withMessages([
+            'email' => 'Invalid email'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
