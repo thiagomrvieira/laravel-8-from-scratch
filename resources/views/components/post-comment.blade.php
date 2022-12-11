92cac311adf4de15d411bc0e78e0513c6ccc83f9
thiagomrvieira <thiagomrvieira@gmail.com>
2022-12-11 12:12:26 +0000

    Make the Comments Section Dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,20 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
         <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold"> John Doe</h3>
+            <h3 class="font-bold"> {{ $comment->author->username }}</h3>
 
             <p class="text-xs">
                 Posted
-                <time>8 months ago </time>
+                <time>{{ $comment->created_at }} </time>
             </p>
         </header>
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Necessitatibus harum sit dolor qui error veritatis ex odit. S
-            equi, eius exercitationem fuga, quasi laboriosam consectetur, 
-            eaque quos laudantium quas voluptatem voluptas?
+            {{ $comment->body }}
         </p>
     </div>
 </article>
